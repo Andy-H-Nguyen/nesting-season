5ca6e2867896440e600d3e6ed8f31dad7e05d661
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-11-16 21:14:28 -0500

    start screens

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
