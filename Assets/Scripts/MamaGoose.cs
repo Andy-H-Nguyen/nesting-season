dda970802d89350161ff8c87c1ca472eea44e89e
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-11-23 21:20:09 -0500

    organize character sprites

--- Assets/Scripts/MamaGoose.cs
+++ Assets/Scripts/MamaGoose.cs
@@ -9,18 +9,23 @@ public class MamaGoose : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = new Vector2 (0, -speed);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			rb.velocity = new Vector2 (speed, 0);
+			GetComponent<Animator>().Play ("MamaGooseWalkRight");
 		} else if (Input.GetKey (KeyCode.DownArrow)) {
 			rb.velocity = new Vector2 (0, -speed);
+			GetComponent<Animator>().Play ("MamaGooseWalkDown");
 		} else if (Input.GetKey (KeyCode.LeftArrow)) {
 			rb.velocity = new Vector2 (-speed, 0);
+			GetComponent<Animator>().Play ("MamaGooseWalkLeft");
 		} else if (Input.GetKey (KeyCode.UpArrow)) {
 			rb.velocity = new Vector2 (0, speed);
+			GetComponent<Animator>().Play ("MamaGooseWalkUp");
 		}
 	}
 }
