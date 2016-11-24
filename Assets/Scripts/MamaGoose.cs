b67a27889b40c3bd8e1947e9d6872c7799ade2d6
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-11-23 20:50:15 -0500

    mother goose anims

---
+++ Assets/Scripts/MamaGoose.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MamaGoose : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.velocity = new Vector2 (speed, 0);
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			rb.velocity = new Vector2 (0, -speed);
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.velocity = new Vector2 (-speed, 0);
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			rb.velocity = new Vector2 (0, speed);
+		}
+	}
+}
