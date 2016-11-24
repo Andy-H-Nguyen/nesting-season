20443ba42abbaf9b067ad0cbfd1fb39172733a4d
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-11-23 22:00:10 -0500

    add student

---
+++ Assets/Scripts/Student.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Student : MonoBehaviour {
+
+	public Transform explosionPrefab;
+
+	void OnCollisionEnter(Collision collision) {
+		ContactPoint contact = collision.contacts[0];
+		Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+		Vector3 pos = contact.point;
+		Instantiate(explosionPrefab, pos, rot);
+		Destroy(gameObject);
+	}
+}
