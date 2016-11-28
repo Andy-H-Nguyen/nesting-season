00ccbb3765c325b4c4d115a3a5f8a398ac2eccee
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-11-28 11:57:55 -0500

    student move scripts

--- Assets/Scripts/Student.cs
+++ Assets/Scripts/Student.cs
@@ -4,6 +4,63 @@ using System.Collections;
 public class Student : MonoBehaviour {
 
 	public Transform explosionPrefab;
+	private Rigidbody2D rb;
+	private bool walkForward; 
+	private bool walkInX; // Walks in Y direction if false
+
+	private float initialX;
+	private float initialY;
+	private float WalkRange = 2.0f; // Range of motion
+	public float speed = 2.0f;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		rb.freezeRotation = true;
+
+		initialX = transform.position.x;
+		initialY = transform.position.y;
+
+		WalkRange = Random.Range (1f, 4f);
+
+		if (Random.Range (0f, 1f) < 0.5f) {
+			walkInX = false;
+		} else {
+			walkInX = true;
+		}
+	}
+
+	void Update () {
+		if (walkInX) {
+			if (walkForward)
+				transform.Translate (Vector2.right * speed * Time.deltaTime);
+			else
+				transform.Translate (-Vector2.right * speed * Time.deltaTime);
+
+			if (transform.position.x >= initialX + WalkRange) {
+				walkForward = false;
+			}
+
+			if (transform.position.x <= initialX - WalkRange) {
+				walkForward = true;
+			}
+		}
+
+		if (!walkInX) {
+			if (walkForward)
+				transform.Translate (Vector2.up * speed * Time.deltaTime);
+			else
+				transform.Translate (-Vector2.up * speed * Time.deltaTime);
+
+			if (transform.position.y >= initialX + WalkRange) {
+				walkForward = false;
+			}
+
+			if (transform.position.y <= initialX - WalkRange) {
+				walkForward = true;
+			}
+		}
+	}
+
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		Instantiate(explosionPrefab, gameObject.transform.position, Quaternion.identity);
