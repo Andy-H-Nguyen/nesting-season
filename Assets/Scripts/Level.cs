2a4e6c426befb0805a198c74eb0156216f6ae491
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-11-28 12:04:18 -0500

    delete old maclab

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour {
+	private int count;
+	private GameObject[] getCount;
+
+	// Use this for initialization
+	void Start () {
+		getCount = GameObject.FindGameObjectsWithTag ("Respawn");
+		count = getCount.Length;
+		Debug.Log (count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
