8b658e12eba951ab265e68c1f691abb7ee8c9551
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-11-30 15:29:17 -0500

    add difficulty

--- Assets/Scripts/CameraFollowingController.cs
+++ Assets/Scripts/CameraFollowingController.cs
@@ -4,23 +4,35 @@ using System.Collections;
 public class CameraFollowingController : MonoBehaviour {
 
 	public GameObject player;       //Public variable to store a reference to the player game object
+	public Transform congaLine;
+	public int baseFov = 60;
+	public int fovIncreaseModifier = 5;
+	public int fovIncreaseRatio = 1;
 
 
 	private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 	private int cameraZoom = 5;
-
+	private Vector3 zoomVector;
 	// Use this for initialization
 	void Start () 
 	{
+		zoomVector = new Vector3 (0, 0, cameraZoom);
 		//Calculate and store the offset value by getting the distance between the player's position and camera's position.
 		offset = transform.position - player.transform.position;
+	}
+
+	void Update () 
+	{
+		int lineSize = congaLine.GetComponent<SnakeMovement> ().GetLength();
+		int newFov = baseFov + fovIncreaseModifier * (lineSize / fovIncreaseRatio);  
+		Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, newFov, Time.deltaTime);
 	}
 
 	// LateUpdate is called after Update each frame
 	void LateUpdate () 
 	{
+
 		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
 		transform.position = player.transform.position + offset;
 	}
-
 }
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,38 +4,59 @@ using System.Collections;
 public class Level : MonoBehaviour {
 	private int count;
 	private GameObject[] getCount;
+	private bool readyToSpawn = false;
 
 	public Transform map;
+	public Transform congaLine;
 	public Transform spawnPrefab; 
 	public Transform effectPrefab;
-	public int maxEnemies = 10;
+	public int maxEnemies = 12;
+	public int distanceFromPlayer = 3;
 
 	// Use this for initialization
 	void Start () {
+		InvokeRepeating("TimedSpawn", 1f, 1f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		getCount = GameObject.FindGameObjectsWithTag ("Enemy");
-		if (getCount.Length < maxEnemies) {
-			Spawn ();
-		}
-		// Debug.Log (getCount.Length);
 	}
 
 	Vector3 GetLocationInMap () {
 		Tiled2Unity.TiledMap mapScript = map.gameObject.GetComponent<Tiled2Unity.TiledMap>();
-		float mapHeight = mapScript.GetMapHeightInPixelsScaled();
-		float mapWidth = mapScript.GetMapWidthInPixelsScaled();
-		float randomX = Mathf.Abs (Random.Range(-(mapWidth/2), (mapWidth/2)));
-		float randomY = Mathf.Abs (Random.Range(-(mapHeight/2), (mapHeight/2)));
+		float mapHeight = mapScript.TileHeight;
+		float mapWidth = mapScript.TileWidth;
+
+		// Get position in the map, approx,
+		float randomX = (Random.Range(map.position.x + 1, map.position.x + mapWidth - 1));
+		float randomY = (Random.Range(map.position.y - 1, map.position.y - mapHeight + 1));
 		return new Vector3 (randomX, randomY, 0);
+	}
+
+	void TimedSpawn() {
+		getCount = GameObject.FindGameObjectsWithTag ("Enemy");
+		if (getCount.Length < maxEnemies) {
+			Spawn ();
+		}
 	}
 
 	void Spawn () {
 		Vector3 spawnLocation = GetLocationInMap ();
-		Debug.Log (spawnLocation.x + spawnLocation.y);
+		while (isCloseToPlayer(spawnLocation)) {
+			spawnLocation = GetLocationInMap ();
+		}
+
+		Debug.Log ("Spawned at: " + spawnLocation.x + ',' + spawnLocation.y);
 		Instantiate (effectPrefab, spawnLocation, Quaternion.identity);
 		Instantiate (spawnPrefab, spawnLocation, Quaternion.identity);
+	}
+
+	bool isCloseToPlayer(Vector3 v) {
+		foreach(Transform t in congaLine.GetComponent<SnakeMovement> ().bodyParts) {
+			if (Vector3.Distance (v, t.position) < distanceFromPlayer) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
