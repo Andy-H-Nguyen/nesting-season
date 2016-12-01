b5fd78720f7e42a067f6b1c89e67a5dbe94eb690
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-12-01 14:44:55 -0500

    title polish

---
+++ Assets/Scripts/MenuSounds.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSounds : MonoBehaviour {
+
+	public AudioSource musicSource;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlaySound() {
+		musicSource.Play();
+	}
+}
